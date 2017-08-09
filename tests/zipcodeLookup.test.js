9a21e9c99c8613a09f3b4a7b3e924c3f6cde7428
Perogi <perogi@infinityfactorial.com>
2017-08-08 20:53:21 -0400

    Fix for Issue: #14. https://github.com/perogi/zipcodes-perogi/issues/14

--- tests/zipcodeLookup.test.js
+++ tests/zipcodeLookup.test.js
@@ -69,7 +69,7 @@ describe('zipcode timezone lookup tests', () => {
 
 
 describe('lookup zipcodes by city, state tests', () => {
-    it("should find by city and state", () => {
+    it("should find by a valid city and state", () => {
         let l = zipcodes.lookupByName("Marion", "il");
         expect(l.length).to.equal(1);
 
@@ -85,17 +85,49 @@ describe('lookup zipcodes by city, state tests', () => {
         l = zipcodes.lookupByName("New York", "NY");
         expect(l.length).to.equal(145);
     });
+
+    it("should not find by blank city", () => {
+        l = zipcodes.lookupByName("", "MA");
+        expect(l.length).to.equal(0);
+    });
+
+    it("should not find by blank state", () => {
+        l = zipcodes.lookupByName("Boston", "");
+        expect(l.length).to.equal(0);
+    });
+
+    it("should not find by undefined city", () => {
+        l = zipcodes.lookupByName(undefined, "MA");
+        expect(l.length).to.equal(0);
+    });
+
+    it("should not find by undefined state", () => {
+        l = zipcodes.lookupByName("Boston", undefined);
+        expect(l.length).to.equal(0);
+    });
 });
 
 describe('lookup cities by state tests', () => {
-    it("should find by state abbreviation", () => {
+    it("should find by a valid state abbreviation", () => {
         let l = zipcodes.lookupByState("RI");
         expect(l.length).to.equal(90);
 
         l = zipcodes.lookupByState("ri");
         expect(l.length).to.equal(90);
+    });
 
+    it("should not find using an invalid state abbreviation", () => {
         l = zipcodes.lookupByState("foobar");
+        expect(l.length).to.equal(0);
+    });
+
+    it("should not find using a blank state abbreviation", () => {
+        l = zipcodes.lookupByState("");
+        expect(l.length).to.equal(0);
+    });
+
+    it("should not find using an undefined state abbreviation", () => {
+        l = zipcodes.lookupByState(undefined);
         expect(l.length).to.equal(0);
     });
 });
